ee4a61b77b1e84eb853f0f3c75cedeaf221f0298
delan <delank@protonmail.com>
2021-03-12 22:43:33 -0700

    normals added

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
-use color::write_color;
-use vec3::{Color, Point3, Vec3};
-use ray::Ray;
+
 pub mod vec3;
 pub mod color;
 pub mod ray;
 
+use color::write_color;
+use vec3::{Color, Point3, Vec3};
+use ray::Ray;
+
+fn hit_sphere(center: Point3, radius: f64, r:Ray) -> f64 {
+    let oc: Vec3 = r.origin() - center;
+    let a = Vec3::dot(r.direction(), r.direction());
+    let b = 2.0 * Vec3::dot(oc, r.direction());
+    let c = Vec3::dot(oc,oc) - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0*a);
+    }
+
+}
+
 fn ray_color(r:Ray) -> Color {
+    let mut t = hit_sphere(Point3::new(0.0,0.0,-1.0), 0.5, r);
+    if t > 0.0 {
+        let n = Vec3::unit_vector(r.at(t) - Vec3::new(0.0,0.0, -1.0));
+        return 0.5 * Color::new(n.x()+1.0, n.y() + 1.0,n.z() + 1.0);
+    }
     let unit_direction = Vec3::unit_vector(r.direction());
-    let t = 0.5*(unit_direction.y()+1.0);
-    (1.0-t) * Color::new(1.0,1.0,1.0) + t * Color::new(0.5, 0.7, 1.0)
+    t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0-t) * Color::new(1.0,1.0,1.0) + t * Color::new(0.5, 0.7, 1.0);
 }
 
 fn main() {
     //Image
     const ASPECT_RATIO: f64 = 16.0/9.0;
     const IMAGE_WIDTH: i32 = 400;
-    const IMAGE_HEIGHT: i32 = IMAGE_WIDTH / (ASPECT_RATIO as i32);
+    const IMAGE_HEIGHT: i32 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as i32;
 
     //Camera
 
