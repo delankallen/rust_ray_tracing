62eccc9bfb89b63b26baf2156aa1d52c3d10dbc2
delan <delank@protonmail.com>
2021-03-15 16:44:59 -0600

    Implemented antialiasing

--- src/color.rs
+++ src/color.rs
@@ -1,10 +1,20 @@
 use crate::vec3::Color;
+use crate::utility_funcs::clamp;
 
-pub fn write_color(pixel_color:Color) {
+pub fn write_color(pixel_color:Color, samples_per_pixel:f64) {
+    let mut r = pixel_color.x();
+    let mut g = pixel_color.y();
+    let mut b = pixel_color.z();
+
+    let scale = 1.0 / samples_per_pixel;
+    r*=scale;
+    g*=scale;
+    b*=scale;
+
     println!(
         "{} {} {}", 
-        (255.999 * pixel_color.x()) as i32, 
-        (255.999 * pixel_color.y()) as i32, 
-        (255.999 * pixel_color.z()) as i32
+        (256.0 * clamp(r, 0.0, 0.999)) as i32, 
+        (256.0 * clamp(g, 0.0, 0.999)) as i32, 
+        (256.0 * clamp(b, 0.0, 0.999)) as i32
     )
 }
--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,28 @@
 pub mod vec3;
 pub mod color;
 pub mod ray;
+mod hittable;
+mod sphere;
+mod hittable_list;
+mod camera;
+mod utility_funcs;
 
+use camera::Camera;
 use color::write_color;
+use hittable_list::HittableList;
 use vec3::{Color, Point3, Vec3};
 use ray::Ray;
+use hittable::Hittable;
+use sphere::Sphere;
 
-fn hit_sphere(center: Point3, radius: f64, r:Ray) -> f64 {
-    let oc: Vec3 = r.origin() - center;
-    let a = Vec3::dot(r.direction(), r.direction());
-    let b = 2.0 * Vec3::dot(oc, r.direction());
-    let c = Vec3::dot(oc,oc) - radius*radius;
-    let discriminant = b*b - 4.0*a*c;
-    if discriminant < 0.0 {
-        return -1.0;
-    } else {
-        return (-b - discriminant.sqrt()) / (2.0*a);
-    }
+use rand::prelude::*;
 
-}
-
-fn ray_color(r:Ray) -> Color {
-    let mut t = hit_sphere(Point3::new(0.0,0.0,-1.0), 0.5, r);
-    if t > 0.0 {
-        let n = Vec3::unit_vector(r.at(t) - Vec3::new(0.0,0.0, -1.0));
-        return 0.5 * Color::new(n.x()+1.0, n.y() + 1.0,n.z() + 1.0);
+fn ray_color(r:&Ray, world:&dyn Hittable) -> Color {
+    if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
+        return 0.5 * (rec.normal + Color::new(1.0,1.0,1.0));
     }
     let unit_direction = Vec3::unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    let t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0-t) * Color::new(1.0,1.0,1.0) + t * Color::new(0.5, 0.7, 1.0);
 }
 
@@ -37,17 +32,23 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0/9.0;
     const IMAGE_WIDTH: i32 = 400;
     const IMAGE_HEIGHT: i32 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as i32;
+    const SAMPLES_PER_PIXEL:i32 = 100;
 
+    //World
+    let mut world = HittableList::new(Vec::new());
+
+    world.add(Box::new(
+        Sphere::new(
+            Point3::new(0.0,0.0,-1.0),
+            0.5
+        )));
+    world.add(Box::new(
+        Sphere::new(
+            Point3::new(0.0,-100.5,-1.0),
+            100.0
+        )));
     //Camera
-
-    let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0,0.0,0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0,0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0,0.0, focal_length);
+    let cam = Camera::new();
 
     //Render
 
@@ -57,11 +58,15 @@ fn main() {
     for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {} ", j);
         for i in 0..IMAGE_WIDTH {
-            let u = (i as f64)/((IMAGE_WIDTH-1) as f64);
-            let v = (j as f64)/((IMAGE_HEIGHT-1) as f64);
-            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            let pixel_color = ray_color(r);
-            write_color(pixel_color);
+            let mut rng = rand::thread_rng();
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let u = (i as f64 + rng.gen::<f64>()) / (IMAGE_WIDTH as f64 - 1.0);
+                let v = (j as f64 + rng.gen::<f64>())/(IMAGE_HEIGHT as f64 - 1.0);
+                let r = cam.get_ray(u,v);
+                pixel_color += ray_color(&r, &world);
+            }
+            write_color(pixel_color, SAMPLES_PER_PIXEL as f64);
         }
     }
 
